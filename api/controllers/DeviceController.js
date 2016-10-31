f9567d446501fe264edeaf2eb8b12f3e874e2827
Alain <alain.attica@adixon.fr>
2016-10-31 23:27:19 +0100

    device

--- api/controllers/DeviceController.js
+++ api/controllers/DeviceController.js
@@ -1,11 +1,46 @@
 /**
- * DeviceController
+ * ParameterController
  *
- * @description :: Server-side logic for managing devices
+ * @description :: Server-side logic for managing Parameters
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
 module.exports = {
 	
-};
+    swop: function (req, res) {
+    	Device.findOne({
+    		identifier: req.param('identifier')
+    	}).exec(function(err, device){
+            
+            if (device) {
+            	// update
+            	Device.update(
+            		device.id, {
+            		posx: req.param('posx'),
+                	posy: req.param('posy'),                	
+	            	}, function(err, deviceUpdated){	                    		            	
+		                return res.ok();		                
+	                });
 
+            } else {
+            	Device.create({
+                	identifier: req.param('identifier'),
+            		posx: req.param('posx'),
+                	posy: req.param('posy'),
+
+                }).exec(function(error, device) {
+                	if (error) {                		
+                		return res.serverError('Error');
+                	};
+                	if (device) {
+                		return res.ok();	                		
+                	};
+                });
+
+            }
+       	});
+        
+    },
+
+    
+};

1aa87d56b7e48a135c95ebd4db931b5cbd80d02c
Alain <batataw@gmail.com>
2016-12-03 02:09:30 +0100

    users

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -134,7 +134,7 @@ module.exports = {
 	            	return res.serverError('LOGIN-FAILED');
 				}
                 else{
-                    return res.ok({id:user.id});
+                    return res.json({id:user.id});
                 }	
 
 			});
